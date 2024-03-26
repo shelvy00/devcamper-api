cd9c2a1be3992d423c2b021371575676924aa6c5
Shelvy <shelvy511@gmail.com>
2024-03-26 12:40:45 -0500

    update

---
+++ middleware/error.js
@@ -0,0 +1,13 @@
+const { off } = require("../models/Bootcamp");
+
+const errorHandler = (err,req,res,next) =>{
+    // Log to console for dev
+    console.log(err.stack.red);
+
+    res.status(500).json({
+        success: false,
+        error: err.message
+    });
+}
+
+module.exports = errorHandler;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const colors = require("colors")
+const errorHandler = require("./middleware/error")
 const connectDB = require("./config/db")
 
 // Load env vars
@@ -14,7 +16,8 @@ const bootcamps = require("./routes/bootcamps");
 
 const app = express();
 
-
+// Body parser
+app.use(express.json());
 
 // Dev logging middleware
 if(process.env.NODE_ENV === "development") {
@@ -24,6 +27,8 @@ if(process.env.NODE_ENV === "development") {
 //Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
 
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 6000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
